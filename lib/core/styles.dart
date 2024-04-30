8a9bd9b9db4e7a3cf3e2d25836c33c9870adf29a
abdallahshaban <abdallahshaban200@gmail.com>
2024-04-30 19:42:05 +0300

    finish getstarted view

---
+++ lib/core/styles.dart
@@ -0,0 +1,35 @@
+
+
+import 'package:flutter/material.dart';
+
+
+class Styles {
+  Styles._();
+
+static TextStyle style20 = const TextStyle(
+  fontSize: 20 , color: Colors.white
+);
+
+static TextStyle style22 = const TextStyle(
+  fontSize: 22 , color: Colors.white
+);
+
+static TextStyle style18 = const TextStyle(
+  fontSize: 18 , color: Colors.white
+);
+
+static TextStyle style16 = const TextStyle(
+  fontSize: 16 , color: Colors.white
+);
+
+static TextStyle style30 = const TextStyle(
+  fontSize: 30 , color: Colors.white,
+  fontWeight: FontWeight.bold
+);
+
+static TextStyle style35 = const TextStyle(
+  fontSize: 35 , color: Colors.white,
+  fontWeight: FontWeight.bold
+);
+}
+
---
+++ lib/features/get started/getstart.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:resturant/constant.dart';
+import 'package:resturant/core/styles.dart';
+import 'package:resturant/features/tips/tips.dart';
+
+class GetStartedView extends StatelessWidget {
+  const GetStartedView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var myHeight = MediaQuery.of(context).size.height / 3 ;
+    return Scaffold(
+      // ignore: avoid_unnecessary_containers
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              height: myHeight *2,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                image: DecorationImage(image: AssetImage('asset/logos/food.png') , fit: BoxFit.contain)
+              ),
+            ),
+
+            Container(
+              height: myHeight,
+              decoration: BoxDecoration(
+                color: primaryColor,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0,3),
+                  ),
+                  
+                ],
+                borderRadius: const BorderRadius.only(topLeft: Radius.circular(20), 
+                topRight: Radius.circular(20)
+                )
+              ),
+              child: ListView(
+                children:  [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Happy Meals' , style: Styles.style35,),
+                        Text('Discover the best foods from over 1,000 resturants.' ,
+                        style: Styles.style18 ,
+                        ),
+                        const SizedBox(height: 20,),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context){
+                              return const TipsView();
+                            }));
+                          },
+                          child: Container(
+                            height: 40,
+                            width: 150,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Center(
+                              child: Text("Get Started" ,
+                               style: Styles.style22.copyWith(color: Colors.black, fontWeight: FontWeight.bold
+                               ),)),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
